32853d17fe16349b08ec905b72e88cdc25dc2a07
puchunjie <puchunjie@live.com>
2017-11-01 17:04:13 +0800

    公共底部样式修改

--- src/components/publicFooter.vue
+++ src/components/publicFooter.vue
@@ -5,12 +5,12 @@
         <p class="left-p" style="margin-top:18px"><span class="iconfont icon-ziyuan6"></span>江苏省无锡市新吴区纺城大道289号南方不锈钢市场1-107</p>
         <p class="left-p"><span class="iconfont icon-ziyuan7"></span>0510-81812186</p>
         <p class="left-p"><span class="iconfont icon-ziyuan8"></span>8:30~17:30（周一至周六）</p>
-
+    
         <div class="qr">
             <img src="http://tbxoss.oss-cn-hangzhou.aliyuncs.com/assets/qr2.png">
             <span>微信公众号</span>
         </div>
-        
+    
         <div class="qr er">
             <img src="http://tbxoss.oss-cn-hangzhou.aliyuncs.com/assets/qr1.png">
             <span>掌上淘不锈</span>
@@ -28,40 +28,42 @@
         overflow: hidden;
         .logo {
             font-size: 65px;
-            color: #fff;
+            color: #ddd;
         }
         .left-p {
             width: 550px;
             padding: 0 10px;
-            color: #FFFFFF;
-            font-size: 14px;
+            color: #ddd;
+            font-size: 12px;
             line-height: 24px;
+            margin-bottom: 5px;
             .iconfont {
                 font-size: 20px;
-                margin-right:5px;
+                margin-right: 5px;
+                vertical-align: sub
             }
         }
-        .qr{
+        .qr {
             position: absolute;
             display: block;
             width: 120px;
             bottom: 40px;
             right: 300px;
-            color: #fff;
+            color: #ddd;
             font-size: 12px;
-            span{
+            span {
                 display: inline-block;
-                margin-top:5px;
+                margin-top: 5px;
             }
-            img{
+            img {
                 display: block;
                 width: 100%;
                 padding: 10px;
                 border: @b_d1;
                 background-color: #fff;
             }
-            &.er{
-                right:120px;
+            &.er {
+                right: 120px;
             }
         }
     }
